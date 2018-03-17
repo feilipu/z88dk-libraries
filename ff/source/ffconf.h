24d904dcff03547db32553537c09461386607229
Phillip Stevens <phillip.stevens@gmail.com>
2018-03-18 09:35:16 +1100

    RC2014 - make read-only

--- ff/source/ffconf.h
+++ ff/source/ffconf.h
@@ -8,7 +8,13 @@
 / Function Configurations
 /---------------------------------------------------------------------------*/
 
+#if __RC2014
+#define FF_FS_READONLY  1
+#elif __YAZ180
 #define FF_FS_READONLY  0
+#else
+#define FF_FS_READONLY  0
+#endif
 /* This option switches read-only configuration. (0:Read/Write or 1:Read-only)
 /  Read-only configuration removes writing API functions, f_write(), f_sync(),
 /  f_unlink(), f_mkdir(), f_chmod(), f_rename(), f_truncate(), f_getfree()
