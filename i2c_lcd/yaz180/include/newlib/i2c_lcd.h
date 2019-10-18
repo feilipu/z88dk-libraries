29344762ea98d8bc44ab9365138939989687f186
Phillip Stevens <phillip.stevens@gmail.com>
2019-10-18 12:40:33 +1100

    i2c_lcd - header update

--- i2c_lcd/yaz180/include/newlib/i2c_lcd.h
+++ i2c_lcd/yaz180/include/newlib/i2c_lcd.h
@@ -462,9 +462,9 @@ enum LCD_FontSort
 enum LCD_FontMode
 {
     FM_ANL_AAA=0x00,    //FM_AutoNewLine_AutoAddrAdd
-    FM_ANL_MAA=0x10,    //FM_AanualNewLine_ManualAddrAdd
-    FM_MNL_MAA=0x30,    //FM_ManualNewLine_ManualAddrAdd
-    FM_MNL_AAA=0x20     //FM_ManualNewLine_AutoAddrAdd
+    FM_ANL_MAA=0x10,    //FM_AutoNewLine_ManualAddrAdd
+    FM_MNL_AAA=0x20,    //FM_ManualNewLine_AutoAddrAdd
+    FM_MNL_MAA=0x30     //FM_ManualNewLine_ManualAddrAdd
 };
 
 enum LCD_WorkingMode
