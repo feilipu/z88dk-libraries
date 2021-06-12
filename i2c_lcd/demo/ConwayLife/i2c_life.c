86a9872176f9c5f07c6c32570b7ecfce1cc56699
Phillip Stevens <phillip.stevens@gmail.com>
2021-06-12 12:35:48 +1000

    i2c_lcd - update Conway Life

--- i2c_lcd/demo/ConwayLife/i2c_life.c
+++ i2c_lcd/demo/ConwayLife/i2c_life.c
@@ -1,8 +1,8 @@
 /*
-    Title:            i2c_life.c
+    Title:          i2c_life.c
     Date Created:   16/10/2019
     Last Modified:  28/10/2019
-    Target:            Zilog Z180
+    Target:         Zilog Z180
     Environment:    z88dk
     Purpose:        Drive Sparking I2C LCD Display
     Application:    Conway's Life
@@ -36,13 +36,13 @@
 */
 
 // ZSDCC
-// zcc +yaz180 -subtype=app -SO3 -v -m --list --math32 -llib/yaz180/i2c_lcd --c-code-in-asm --max-allocs-per-node100000 i2c_life.c -o i2c_life -create-app
+// zcc +yaz180 -subtype=app -v -m -SO3 --list --math32 -llib/yaz180/i2c_lcd --c-code-in-asm --max-allocs-per-node200000 i2c_life.c -o i2c_life -create-app
 
-// zcc +yaz180 -subtype=cpm -SO3 -v -m --list --math32 -llib/yaz180/i2c_lcd --c-code-in-asm --max-allocs-per-node100000 i2c_life.c -o i2c_life -create-app
+// zcc +yaz180 -subtype=cpm -v -m -SO3 --list --math32 -llib/yaz180/i2c_lcd --c-code-in-asm --max-allocs-per-node200000 i2c_life.c -o i2c_life -create-app
 
 
 // SCCZ80
-// zcc +yaz180 -subtype=app -clib=new -SO3 -v -m --list --math32 -llib/yaz180/i2c_lcd i2c_life.c -o i2c_life -create-app
+// zcc +yaz180 -subtype=app -clib=new -v -m -SO3 --list --math32 -llib/yaz180/i2c_lcd i2c_life.c -o i2c_life -create-app
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -62,31 +62,33 @@
 
 /////////////////////////////////////////////////////////////////////////
 
-#pragma printf = "%li %lu %u" // enables %li %lu %u only
+#pragma printf = "%li %lu %s %u" // enables %li %lu %s, %u only
 
 /////////////////////////////////////////////////////////////////////////
 
 // Define the size of the screen in pixels
 // Origin [0,0] is top left of screen
 
-#define CELLS_X 128
-#define CELLS_Y 64
+#define CELLS_X LCD_X_SIZE_MAX
+#define CELLS_Y LCD_Y_SIZE_MAX
+
+#define CELL_TOTAL_BYTES (CELLS_X*CELLS_Y/8)
 
 /////////////////////////////////////////////////////////////////////////
 
 extern uint8_t LCD_Port;    // Global for PCA9665 Device Port MSB
 
-static uint8_t current_generation[CELLS_X*CELLS_Y/8];
-static uint8_t old_generation[CELLS_X*CELLS_Y/8];
+static uint8_t current_generation[CELL_TOTAL_BYTES];
+static uint8_t old_generation[CELL_TOTAL_BYTES];
 
 /////////////////////////////////////////////////////////////////////////
 
-inline uint8_t get_cell(uint8_t *from, uint8_t x, uint8_t y)
+uint8_t get_cell(uint8_t *from, uint8_t x, uint8_t y)
 {
-    x &= (CELLS_X-1);   // ensure x < 128 this is non-portable (but fast)
-    y &= (CELLS_Y-1);   // ensure y < 64 this is non-portable (but fast)
+    x &= (uint8_t)(CELLS_X-1);   // ensure x < 128 this is non-portable (but fast)
+    y &= (uint8_t)(CELLS_Y-1);   // ensure y < 64 this is non-portable (but fast)
 
-    return from[(uint16_t)x+(uint16_t)y*(CELLS_X/8)] & (uint8_t)(1 << y%8);
+    return (from[x + (y>>3)*CELLS_X] & (uint8_t)(1 << y%8)) ? 1 : 0;
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -95,11 +97,11 @@ uint16_t get_total(uint8_t *from)
 {
     uint16_t total = 0;
 
-    for(uint8_t x=0; x < CELLS_X; ++x)
+    for (uint8_t x=0; x < CELLS_X; ++x)
     {
-        for(uint8_t y=0; y < CELLS_Y; ++y)
+        for (uint8_t y=0; y < CELLS_Y; ++y)
         {
-            if(get_cell(from,x,y) != 0) ++total;
+            if (get_cell(from,x,y)) ++total;
         }
     }
     return total;
@@ -109,24 +111,24 @@ uint16_t get_total(uint8_t *from)
 
 void set_cell(uint8_t *to, uint8_t x, uint8_t y, uint8_t value)
 {
-    if(value)
-        to[(uint16_t)x+(uint16_t)y*(CELLS_X/8)] |= (uint8_t)(1 << y%8);
+    if (value)
+        to[x + (y>>3)*CELLS_X] |= (uint8_t)(1 << y%8);
     else
-        to[(uint16_t)x+(uint16_t)y*(CELLS_X/8)] &= ~( (uint8_t)(1 << y%8) );
+        to[x + (y>>3)*CELLS_X] &= (uint8_t)~(1 << y%8);
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-inline void fill_cell(uint8_t *to, uint8_t x, uint8_t y)
+void fill_cell(uint8_t *to, uint8_t x, uint8_t y)
 {
-    to[(uint16_t)x+(uint16_t)y*(CELLS_X/8)] |= (uint8_t)(1 << y%8);
+    to[x + (y>>3)*CELLS_X] |= (uint8_t)(1 << y%8);
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-inline void clear_cell(uint8_t *to, uint8_t x, uint8_t y)
+void clear_cell(uint8_t *to, uint8_t x, uint8_t y)
 {
-    to[(uint16_t)x+(uint16_t)y*(CELLS_X/8)] &= ~( (uint8_t)(1 << y%8) );
+    to[x + (y>>3)*CELLS_X] &= (uint8_t)~(1 << y%8);
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -135,58 +137,34 @@ void fill_random_data(uint8_t *to)
 {
     // random seed generator is fractions of a second
     srand( (uint16_t)_system_time ^ _system_time_fraction );
-    
-    for (uint16_t neighbours = 0; neighbours < (CELLS_X*CELLS_Y/8); ++neighbours)
+
+    for (uint16_t cell = 0; cell < CELL_TOTAL_BYTES; ++cell)
     {
-           to[neighbours] = (uint8_t)rand();
+           to[cell] = (uint8_t)rand();
     }
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-inline void clear_data(uint8_t *to)
+void clear_data(uint8_t *to)
 {
-    memset( to, 0, CELLS_X*CELLS_Y/8);
+    memset( to, 0, CELL_TOTAL_BYTES );
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-inline void copy_old_new(uint8_t *to, uint8_t *from)
+void copy_old_new(uint8_t *to, uint8_t *from)
 {
-    memcpy( to, from, CELLS_X*CELLS_Y/8);
+    memcpy_far( to, 0, from, 0, CELL_TOTAL_BYTES );
 }
 
 /////////////////////////////////////////////////////////////////////////
 
 uint8_t get_neighbours(uint8_t *from, uint8_t x, uint8_t y)
 {
-    uint8_t out = 0;
-
-    if(get_cell(from,x-1,y-1))
-        ++out;
-
-    if(get_cell(from,x-1,y))
-        ++out;
-
-    if(get_cell(from,x-1,y+1))
-        ++out;
-
-    if(get_cell(from,x,y-1))
-        ++out;
-
-    if(get_cell(from,x,y+1))
-        ++out;
-
-    if(get_cell(from,x+1,y-1))
-        ++out;
-
-    if(get_cell(from,x+1,y))
-        ++out;
-
-    if(get_cell(from,x+1,y+1))
-        ++out;
-
-    return out;
+    return   get_cell(from,x-1,y-1) + get_cell(from,x-1,y) + get_cell(from,x-1,y+1) \
+           + get_cell(from,x  ,y-1) +                        get_cell(from,x  ,y+1) \
+           + get_cell(from,x+1,y-1) + get_cell(from,x+1,y) + get_cell(from,x+1,y+1);
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -195,11 +173,11 @@ uint8_t get_difference(uint8_t *a,uint8_t *b)
 {
     uint16_t diff=0;
 
-    for(uint8_t  x=0; x < CELLS_X; ++x)
+    for (uint8_t  x=0; x < CELLS_X; ++x)
     {
-        for(uint8_t  y=0; y < CELLS_Y; ++y)
+        for (uint8_t  y=0; y < CELLS_Y; ++y)
         {
-            if((get_cell(a,x,y) && !get_cell(b,x,y)) || (!get_cell(a,x,y) && get_cell(b,x,y)))
+            if ((get_cell(a,x,y) && !get_cell(b,x,y)) || (!get_cell(a,x,y) && get_cell(b,x,y)))
                 ++diff;
         }
     }
@@ -210,7 +188,14 @@ uint8_t get_difference(uint8_t *a,uint8_t *b)
 
 void display(uint8_t *from)
 {
-    LCD_WriteSeriesDispRAM( from, CELLS_X*CELLS_Y/8, 0, 0);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*0], CELLS_X, 0, 0);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*1], CELLS_X, 0, 1);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*2], CELLS_X, 0, 2);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*3], CELLS_X, 0, 3);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*4], CELLS_X, 0, 4);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*5], CELLS_X, 0, 5);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*6], CELLS_X, 0, 6);
+    LCD_WriteSeriesDispRAM( &from[CELLS_X*7], CELLS_X, 0, 7);
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -221,41 +206,40 @@ void main(void)
     
     struct timespec startTime, endTime, resTime;
 
-    clock_gettime(CLOCK_REALTIME, &startTime);
-    
-    io_pio_control = __IO_PIO_CNTL_00;      // enable the 82C55 for output on Port B.
-    io_pio_port_b = 0x00;
-    
+    clock_gettime(CLOCK_REALTIME,&startTime);
+
     LCD_Init(I2C2_PORT);
-    printf("LCD life on I2C port: %u\n", (LCD_Port == I2C1_PORT ? 1 : 2));
+    printf("LCD life on I2C port: %u\n", (LCD_Port == __IO_I2C1_PORT_MSB ? 1 : 2));
 
     LCD_WorkingModeConf(ON, ON, WM_RamMode);// Start RAM mode (characters won't work anymore).    
 
     fill_random_data(old_generation);
     display(old_generation);
 
-    while(1)    //Main Loop
+    while (1)    //Main Loop
     {
-        for(uint8_t x=0; x < CELLS_X; ++x)
+        for (uint8_t x=0; x < CELLS_X; ++x)
         {
-            for(uint8_t y=0; y < CELLS_Y; ++y)        // Inner loop; should be made quick as possible.
+            for (uint8_t y=0; y < CELLS_Y; ++y)        // Inner loop; should be made quick as possible.
             {
                 uint8_t neighbours;
 
                 neighbours = get_neighbours(old_generation, x, y);
 
-                if(neighbours < 2 || neighbours > 3)
+                if (neighbours < 2 || neighbours > 3)
                     clear_cell(current_generation, x, y);
 
-                else if(neighbours == 3)
+                if (neighbours == 3)
                     fill_cell(current_generation, x, y);
             }
         }
 
         display(current_generation);
+        printf("Generation: %u\n", ++generations );
 
         // Exit after a few generations
-        if( ++generations > 0xFF ) break;        
+        if ( generations > 512 ) break;
+
 #if 0
         // Boringness detector:
         if( get_difference(old_generation,current_generation) < 8 || get_total(current_generation) < 6)
@@ -282,7 +266,6 @@ void main(void)
         }
 #endif
         copy_old_new(old_generation, current_generation);
-        io_pio_port_b = (uint8_t)generations;
     }
 
     clock_gettime(CLOCK_REALTIME,&endTime);
