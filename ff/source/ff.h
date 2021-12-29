8b71ab05f29fb907e9e9f5cc4790712f2aaf78cc
Phillip Stevens <phillip.stevens@gmail.com>
2021-12-29 22:17:28 +1100

    ff - update sccz80 ff.h

--- ff/source/ff.h
+++ ff/source/ff.h
@@ -312,6 +312,48 @@ typedef enum {
 /*--------------------------------------------------------------*/
 /* FatFs module application interface                           */
 
+#ifdef __SCCZ80
+
+FRESULT  f_open(FIL* fp,const TCHAR* path,BYTE mode) __smallc;          /* Open or create a file */
+FRESULT  f_close(FIL* fp) __smallc;                                     /* Close an open file object */
+FRESULT  f_read(FIL* fp,void* buff,UINT btr,UINT* br) __smallc;         /* Read data from the file */
+FRESULT  f_write(FIL* fp,const void* buff,UINT btw,UINT* bw) __smallc;  /* Write data to the file */
+FRESULT  f_lseek(FIL* fp,FSIZE_t ofs) __smallc;                         /* Move file pointer of the file object */
+FRESULT  f_truncate(FIL* fp) __smallc;                                  /* Truncate the file */
+FRESULT  f_sync(FIL* fp) __smallc;                                      /* Flush cached data of the writing file */
+FRESULT  f_opendir(DIR* dp,const TCHAR* path) __smallc;                 /* Open a directory */
+FRESULT  f_closedir(DIR* dp) __smallc;                                  /* Close an open directory */
+FRESULT  f_readdir(DIR* dp,FILINFO* fno) __smallc;                      /* Read a directory item */
+FRESULT  f_findfirst(DIR* dp,FILINFO* fno,const TCHAR* path,const TCHAR* pattern) __smallc; /* Find first file */
+FRESULT  f_findnext(DIR* dp,FILINFO* fno) __smallc;                     /* Find next file */
+FRESULT  f_mkdir(const TCHAR* path) __smallc;                           /* Create a sub directory */
+FRESULT  f_unlink(const TCHAR* path) __smallc;                          /* Delete an existing file or directory */
+FRESULT  f_rename(const TCHAR* path_old,const TCHAR* path_new) __smallc;    /* Rename/Move a file or directory */
+FRESULT  f_stat(const TCHAR* path,FILINFO* fno) __smallc;               /* Get file status */
+FRESULT  f_chmod(const TCHAR* path,BYTE attr,BYTE mask) __smallc;       /* Change attribute of a file/dir */
+FRESULT  f_utime(const TCHAR* path,const FILINFO* fno) __smallc;        /* Change timestamp of a file/dir */
+FRESULT  f_chdir(const TCHAR* path) __smallc;                           /* Change current directory */
+FRESULT  f_chdrive(const TCHAR* path) __smallc;                         /* Change current drive */
+FRESULT  f_getcwd(TCHAR* buff,UINT len) __smallc;                       /* Get current directory */
+FRESULT  f_getfree(const TCHAR* path,DWORD* nclst,FATFS** fatfs) __smallc;  /* Get number of free clusters on the drive */
+FRESULT  f_getlabel(const TCHAR* path,TCHAR* label,DWORD* vsn) __smallc;    /* Get volume label */
+FRESULT  f_setlabel(const TCHAR* label) __smallc;                       /* Set volume label */
+FRESULT  f_forward(FIL* fp,UINT(*func)(const BYTE*,UINT),UINT btf,UINT* bf) __smallc;   /* Forward data to the stream */
+FRESULT  f_expand(FIL* fp,FSIZE_t fsz,BYTE opt) __smallc;               /* Allocate a contiguous block to the file */
+FRESULT  f_mount(FATFS* fs,const TCHAR* path,BYTE opt) __smallc;        /* Mount/Unmount a logical drive */
+FRESULT  f_mkfs(const TCHAR* path,const MKFS_PARM* opt,void* work,UINT len) __smallc;   /* Create a FAT volume */
+FRESULT  f_fdisk(BYTE pdrv,const LBA_t ptbl[],void* work) __smallc;     /* Divide a physical drive into some partitions */
+FRESULT  f_setcp(WORD cp) __smallc;                                     /* Set current code page */
+
+int  f_putc(TCHAR c,FIL* fp) __smallc;                                  /* Put a character to the file */
+int  f_puts(const TCHAR* str,FIL* cp) __smallc;                         /* Put a string to the file */
+int  f_printf(FIL* fp,const TCHAR* str,...) __smallc;                   /* Put a formatted string to the file */
+TCHAR  *f_gets(TCHAR* buff,int len,FIL* fp) __smallc;                   /* Get a string from the file */
+
+#endif
+
+#ifdef __SDCC
+
 FRESULT f_open (FIL* fp, const TCHAR* path, BYTE mode);                 /* Open or create a file */
 FRESULT f_close (FIL* fp);                                              /* Close an open file object */
 FRESULT f_read (FIL* fp, void* buff, UINT btr, UINT* br);               /* Read data from the file */
@@ -347,6 +389,8 @@ int f_putc (TCHAR c, FIL* fp);                                          /* Put a
 int f_puts (const TCHAR* str, FIL* cp);                                 /* Put a string to the file */
 int f_printf (FIL* fp, const TCHAR* str, ...);                          /* Put a formatted string to the file */
 TCHAR* f_gets (TCHAR* buff, int len, FIL* fp);                          /* Get a string from the file */
+
+#endif
 
 #define f_eof(fp) ((int)((fp)->fptr == (fp)->obj.objsize))
 #define f_error(fp) ((fp)->err)
