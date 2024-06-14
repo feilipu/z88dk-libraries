52c67cfa0ed9def25b9c53dc9e97fcc192d1540a
Phillip Stevens <phillip.stevens@gmail.com>
2024-06-14 15:03:00 +1000

    regis - add 8085 demo

--- regis/demo/regis_demo.c
+++ regis/demo/regis_demo.c
@@ -8,13 +8,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#if __8085
+#include <_DEVELOPMENT/sccz80/lib/cpm/regis.h>
+#elif __RC2014
 #include <lib/rc2014/regis.h>
+#elif __YAZ180
+#include <lib/yaz180/regis.h>
+#elif __CPM
+#include <lib/cpm/regis.h>
+#endif
 
 // ZSDCC compile
 // zcc +rc2014 -subtype=cpm -v -m --list -llib/rc2014/regis --max-allocs-per-node100000 regis_demo.c -o regis -create-app
 
 // SCCZ80 compile
 // zcc +rc2014 -subtype=cpm -clib=new -v -m -O2 --list -llib/rc2014/regis regis_demo.c -o regis -create-app
+
+// SCCZ80 8085_AM9511 compile
+// zcc +cpm -clib=8085 -v -m --list -O2 -DAMALLOC --am9511 -l../../libsrc/_DEVELOPMENT/lib/sccz80/lib/cpm/regis_8085 regis_demo.c -o demo85 -create-app
 
 // Expected output (where ^ is ESC character).
 // ^P1pS(E)W(I(M))P[600,200]V[][-200,+200]V[][400,100]W(I(G))P[700,100]V(B)[+050,][,+050][-050,](E)V(W(S1))(B)[-100,][,-050][+100,](E)V(W(S1,E))(B)[-050,][,-025][+050,](E)W(I(C))P[200,100]C(A-180)[+100]C(A+180)[+050]W(I(B))P[200,300]C(W(S1))[+100]C(W(S1,E))[+050]W(I(W))T(S02)"hello world"^\
